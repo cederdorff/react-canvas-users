35ad59cffed2dd460cc9ab6bbd8daab9393d8900
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-09-01 14:13:02 +0200

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import User from "./components/User";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [users, setUsers] = useState([]);
+    const [usersToDisplay, setUsersToDisplay] = useState([]);
+    const [showTeachers, setShowTeachers] = useState(true);
+    const [searchValue, setSearchValue] = useState("");
+    const [sortBy, setSortBy] = useState("name");
+
+    useEffect(() => {
+        async function getUsers() {
+            const response = await fetch("https://raw.githubusercontent.com/cederdorff/web-frontend/main/data/wu-e22a.json");
+            const data = await response.json();
+            setUsers(data);
+        }
+
+        getUsers();
+    }, []);
+
+    useEffect(() => {
+        let result = [...users]; // cope the users array
+        if (!showTeachers) {
+            result = result.filter(user => user.enrollment_type === "Student");
+        }
+
+        if (searchValue) {
+            result = result.filter(user => user.name.toLowerCase().includes(searchValue.toLowerCase()));
+        }
+
+        if (sortBy === "sortable_name") {
+            result = result.sort((user1, user2) => user1.sortable_name.localeCompare(user2.sortable_name));
+        } else if (sortBy === "name") {
+            result = result.sort((user1, user2) => user1.name.localeCompare(user2.name));
+        }
+
+        setUsersToDisplay(result);
+    }, [searchValue, showTeachers, sortBy, users]);
+
+    return (
+        <>
+            <header>
+                <h1>Canvas Users</h1>
+                <label>
+                    Show Teachers
+                    <input type="checkbox" checked={showTeachers} onChange={() => setShowTeachers(!showTeachers)} />
+                </label>
+                <label>
+                    Search
+                    <input type="search" placeholder="Search by name" onChange={e => setSearchValue(e.target.value)} />
+                </label>
+                <label>
+                    Sort by
+                    <select onChange={e => setSortBy(e.target.value)}>
+                        <option value="name">First name</option>
+                        <option value="sortable_name">Last name</option>
+                    </select>
+                </label>
+            </header>
+            <main>
+                <section className="grid-container">
+                    {usersToDisplay.map(user => (
+                        <User key={user.id} user={user} />
+                    ))}
+                </section>
+            </main>
+        </>
+    );
 }
 
 export default App;
